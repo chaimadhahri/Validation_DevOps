f2fae0432f494db898d4efcaea51b17e929cd608
chaimadhahri <chayma.dhahri@esprit.tn>
2023-11-08 00:29:28 +0100

    Sonarr

--- src/test/java/tn/esprit/spring/serviceTest/CourseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/serviceTest/CourseServiceImplTest.java
@@ -6,29 +6,75 @@ import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.entities.Course;
 import tn.esprit.spring.entities.Support;
 import tn.esprit.spring.entities.TypeCourse;
+import tn.esprit.spring.repositories.ICourseRepository;
+import tn.esprit.spring.services.CourseServicesImpl;
 import tn.esprit.spring.services.ICourseServices;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.TypeCourse;
+import tn.esprit.spring.repositories.ICourseRepository;
+import tn.esprit.spring.services.CourseServicesImpl;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.text.ParseException;
 import java.util.List;
 
 @SpringBootTest
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class CourseServiceImplTest {
+class CourseServicesImplTest {
+
     @Autowired
-    ICourseServices iCourseServices;
+    private ICourseRepository courseRepository;
+
+    @Autowired
+    private CourseServicesImpl courseServices;
 
     @Test
-    @Order(1)
-    public void testRetrieveAllCourses() {
-        List<Course> listCourses = iCourseServices.retrieveAllCourses();
-        Assertions.assertEquals(listCourses.size(), listCourses.size()); //listCourses.size()
+    void testRetrieveAllCourses() {
+        Course course1 = new Course(1L, 1, TypeCourse.COLLECTIVE_ADULT, Support.SKI, 100.0f, 60);
+        Course course2 = new Course(2L, 2, TypeCourse.COLLECTIVE_ADULT, Support.SKI, 150.0f, 90);
+
+        courseRepository.save(course1);
+        courseRepository.save(course2);
+
+        List<Course> result = courseServices.retrieveAllCourses();
+
+        assertEquals(2, result.size());
+        assertEquals(course1.getNumCourse(), result.get(0).getNumCourse());
+        assertEquals(course2.getNumCourse(), result.get(1).getNumCourse());
     }
 
     @Test
-    @Order(2)
-    public void testAddCourse() {
-        Course course = new Course(1L,2, TypeCourse.INDIVIDUAL, Support.SKI, 20f, 3);
-        Course course1 = iCourseServices.addCourse(course);
-        Assertions.assertEquals(course.getNumCourse(), course1.getNumCourse());
+    void testAddCourse() {
+        Course course = new Course(1L, 1, TypeCourse.INDIVIDUAL, Support.SNOWBOARD, 100.0f, 60);
+
+        Course result = courseServices.addCourse(course);
+
+        assertEquals(course.getNumCourse(), result.getNumCourse());
+    }
+
+    @Test
+    void testUpdateCourse() {
+        Course course = new Course(1L, 1, TypeCourse.INDIVIDUAL, Support.SNOWBOARD, 100.0f, 60);
+
+        courseRepository.save(course);
+
+        Course result = courseServices.updateCourse(course);
+
+        assertEquals(course.getNumCourse(), result.getNumCourse());
+    }
+
+    @Test
+    void testRetrieveCourse() {
+        Course course = new Course(1L, 1, TypeCourse.INDIVIDUAL, Support.SNOWBOARD, 100.0f, 60);
+
+        courseRepository.save(course);
+
+        Course result = courseServices.retrieveCourse(1L);
+
+        assertEquals(course.getNumCourse(), result.getNumCourse());
     }
 }
